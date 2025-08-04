57e768e31fc6ecec444a8481455132d767b2c479
Sergey Kudashev <kudashevs@gmail.com>
2025-08-04 19:39:53 +0200

    Update Employee test suite

--- tests/birthday_greetings/EmployeeTest.php
+++ tests/birthday_greetings/EmployeeTest.php
@@ -2,15 +2,17 @@
 
 namespace birthday_greetings;
 
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 class EmployeeTest extends TestCase
 {
-    public function testBirthday(): void
+    #[Test]
+    public function it_can_identify_a_birthday_day(): void
     {
-        $employee = new Employee("foo", "bar", "1990/01/31", "a@b.c");
-        $this->assertTrue($employee->isBirthday(new XDate("2008/01/31")));
-        $this->assertFalse($employee->isBirthday(new XDate("2008/01/30")));
-        $this->assertFalse($employee->isBirthday(new XDate("2008/02/01")));
+        $employee = new Employee('foo', 'bar', '1990/01/31', 'a@b.c');
+        $this->assertTrue($employee->isBirthday(new XDate('2008/01/31')));
+        $this->assertFalse($employee->isBirthday(new XDate('2008/01/30')));
+        $this->assertFalse($employee->isBirthday(new XDate('2008/02/01')));
     }
 } 
