3abbdd850a50ea81919927fd585dfca0872eaec4
Sergey Kudashev <kudashevs@gmail.com>
2025-08-04 19:39:30 +0200

    Update XDate test suite

--- tests/birthday_greetings/XDateTest.php
+++ tests/birthday_greetings/XDateTest.php
@@ -2,34 +2,39 @@
 
 namespace birthday_greetings;
 
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 class XDateTest extends TestCase
 {
-    public function testGetters(): void
+    #[Test]
+    public function it_can_parse_a_date(): void
     {
-        $date = new XDate("1789/01/14");
+        $date = new XDate("1989/01/14");
         $this->assertEquals(14, $date->getDay());
         $this->assertEquals(1, $date->getMonth());
     }
 
-    public function testIsSameDayWhenIsTrue(): void
+    #[Test]
+    public function it_can_identify_same_dates(): void
     {
-        $date = new XDate("1789/01/14");
-        $sameDay = new XDate("2001/01/14");
-        $differentDay = new XDate("1789/01/15");
-        $differentMonth = new XDate("1789/02/14");
+        $date = new XDate('1989/01/14');
+        $sameDay = new XDate('2001/01/14');
+        $differentDay = new XDate('1989/01/15');
+        $differentMonth = new XDate('1989/02/14');
 
+        // @note remove different
         $this->assertTrue($date->isSameDay($sameDay));
         $this->assertFalse($date->isSameDay($differentDay));
         $this->assertFalse($date->isSameDay($differentMonth));
     }
 
-    public function testIsSameDayWhenIsFalse(): void
+    #[Test]
+    public function it_can_identify_different_dates(): void
     {
-        $date = new XDate("1789/01/14");
-        $differentDay = new XDate("1789/01/15");
-        $differentMonth = new XDate("1789/02/14");
+        $date = new XDate('1989/01/14');
+        $differentDay = new XDate('1989/01/15');
+        $differentMonth = new XDate('1989/02/14');
 
         $this->assertFalse($date->isSameDay($differentDay));
         $this->assertFalse($date->isSameDay($differentMonth));
